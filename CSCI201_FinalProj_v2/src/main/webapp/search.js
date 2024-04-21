0cb36fbeb6cc63cb88d6c3a605853744629074a7
Nicholas Yu <ncyu@usc.edu>
2024-04-21 16:43:20 -0700

    added handleResult function

--- CSCI201_FinalProj_v2/src/main/webapp/search.js
+++ CSCI201_FinalProj_v2/src/main/webapp/search.js
@@ -4,13 +4,17 @@
     event.preventDefault();
     
     const movieName = document.getElementsByClassName('search-input');
+    let movie = {
+		INSERT_MOVIE_TITLE_PARAM_NAME: movieName
+	};
     const url = '/GetMovie'
     
     fetch(url, {
 		method: 'GET',
 		headers:{
 			'Content-Type': 'application/json'
-		}
+		},
+		body: movie
 	})
 	.then((response) => {
         if (!response.ok) {
@@ -29,12 +33,34 @@
     console.log('Search form submitted');
   }
 
-  function handleResultClick(event) {
-    // Handle result item click
+  function handleResult(title) {
+	 let movie = {
+		INSERT_MOVIE_TITLE_PARAM_NAME: title
+	};
+	 const url = '/GetMovie'
+    
+    fetch(url, {
+		method: 'GET',
+		headers:{
+			'Content-Type': 'application/json'
+		},
+		body: movie
+	})
+	.then((response) => {
+        if (!response.ok) {
+            throw new Error("Couldn't fetch search movies");
+        }
+        return response.json();
+    })
+    .then(data =>{
+		let movieList = data.data;
+		
+	})
+	.catch( (error) =>{
+            console.log("request failed", error);
+        });
+    
+	
     console.log('Result item clicked');
   }
 
-  function handleViewMoreClick() {
-    // Handle view more button click
-    console.log('View more button clicked');
-  }
