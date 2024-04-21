f9b0c624a450dc55e58c39189315a0afb23cf61c
Fabrizio Marsano <marsanoj@usc.edu>
2024-04-20 20:33:43 -0700

    bunch of servlets

---
+++ CSCI201_FinalProj_v2/src/main/java/CSCI201_FinalProject/MovieCollection.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+class MovieCollection {
+	
+	
+	private List<Movie> data;
+
+	public List<Movie> getData() {
+		return data;
+	}
+
+	public void setData(List<Movie> data) {
+		this.data = data;
+	}
+	
+	
+	
+}
