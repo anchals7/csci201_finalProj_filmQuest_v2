00d95048451e87e7b16b00fbf8f9553695037c63
Nicholas Yu <ncyu@usc.edu>
2024-04-21 16:09:49 -0700

    cleaned up code

--- CSCI201_FinalProj_v2/src/main/webapp/search.js
+++ CSCI201_FinalProj_v2/src/main/webapp/search.js
@@ -1,16 +1,31 @@
- function handleProfileClick() {
-    // Handle profile link click
-    console.log('Profile link clicked');
-  }
 
-  function handleSearchClick() {
-    // Handle search link click
-    console.log('Search link clicked');
-  }
 
   function handleSearchSubmit(event) {
     event.preventDefault();
-    // Handle search form submit
+    
+    const movieName = document.getElementsByClassName('search-input');
+    const url = '/GetMovie'
+    
+    fetch(url, {
+		method: 'GET',
+		headers:{
+			'Content-Type': 'application/json'
+		}
+	})
+	.then((response) => {
+        if (!response.ok) {
+            throw new Error("Couldn't fetch search movies");
+        }
+        return response.json();
+    })
+    .then(data =>{
+		let movieList = data.data;
+		
+	})
+	.catch( (error) =>{
+            console.log("request failed", error);
+        });
+    
     console.log('Search form submitted');
   }
 
