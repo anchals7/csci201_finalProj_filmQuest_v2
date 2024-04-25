df978e9a6366d4aafdbe8bf561e452ee3358e624
anchals7 <anchalsr@usc.edu>
2024-04-25 16:45:56 -0700

    Search Functionality (w SQL)

--- CSCI201_FinalProj_v2/src/main/webapp/search.js
+++ CSCI201_FinalProj_v2/src/main/webapp/search.js
@@ -1,11 +1,48 @@
 
+/*function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // Get form data
+    const formData = new FormData(document.getElementById('myForm'));
+    const movieName = document.getElementById('myForm');
+    const movieName2 = document.getElementById('myForm').value;
+    
+    console.log(formData);
+    console.log(movieName);
+    console.log(movieName2);
+
+    // Send AJAX request
+    fetch('/MovieSearchServlet', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to submit form');
+        }
+        return response.text(); // Return response text
+    })
+    .then(data => {
+        // Update specific portion of the page with response data
+        document.getElementById('response').innerText = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}*/
+
+// Attach event listener to the form
+//document.getElementById('myForm').addEventListener('submit', handleFormSubmit);
+
+
+/*
 
   function handleSearchSubmit(event) {
     event.preventDefault();
     
-    const movieName = document.getElementsByClassName('search-input').value;
+    const movieName = document.getElementsByClassName('.search-input').value;
     let parameters = new URLSearchParams();
-    parameters.append("INSERT_MOVIE_TITLE_PARAM_NAME", movieName);
+    parameters.append("Title", movieName);
    
     const url = '/MovieSearchServlet?' + parameters.toString();
     
@@ -39,10 +76,46 @@
         });
     
     console.log('Search form submitted');
-  }
+  }*/
+  
+  function loadData() {
+			//var formData = new FormData(document.getElementById("myform"));
+			var xhttp = new XMLHttpRequest();
+			xhttp.open("POST", "MovieSearchServlet", true);
+			xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xhttp.onload = function () {
+				document.getElementById("response").innerHTML = this.responseText;
+				console.log(this.responseText);
+			}
+			xhttp.send("field=title&title=" + document.myForm.title.value);
+		}
+  
+  /*function handleSearchSubmit(event) {
+    event.preventDefault();
+    
+    const movieName = document.querySelector('.search-input').value;
+    console.log(movieName);
+    const url = '/MovieSearchServlet?title=' + encodeURIComponent(movieName);
+    
+    fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Couldn't fetch search movies");
+        }
+        return response.text(); // Assuming the response is text/html
+    })
+    .then(html => {
+        const resultGrid = document.querySelector('.resultsGrid');
+        resultGrid.innerHTML = html; // Update the DOM with search results
+    })
+    .catch(error => {
+        console.error("Request failed:", error);
+        alert("Fetching Movie Search Failed");
+    });
+}*/
   
   
-  function displayMovieTitles(movies){
+  /*function displayMovieTitles(movies){
 		for(let i = 0; i < Math.min(movies.length, 3); i++){
 			let movie = movies[i];
 			let newCard = document.getElementById('titleColTemplate').cloneNode(true);
@@ -53,7 +126,7 @@
 		}
 	
   }
-  
+  */
   
   
 
